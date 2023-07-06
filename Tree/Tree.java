e833285799ef912cc90921fb3e0b4b00ce1c75b6
smuushi <mishih780@gmail.com>
2023-07-06 13:52:54 -0400

    tree print

--- Tree/Tree.java
+++ Tree/Tree.java
@@ -2,6 +2,14 @@ public class Tree {
     public static void main(String[] args) {
         // papaya code... 
         System.out.println("gonna print a tree here..");
-        
+
+
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.println(" *** ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+
     }
 }
