0c27e36ed94994b242db7c75ddf4843604a40d91
smuushi <mishih780@gmail.com>
2023-07-06 17:06:06 -0400

    work

---
+++ Story/Story.java
@@ -0,0 +1,35 @@
+public class Story {
+    /*
+    Your description here
+    */
+        public static void main(String[] args){
+        
+        /* 
+          this program is a madlib pracice with variables
+        */
+  
+  
+        String name1 = "michael";
+        String adjective1 = "ok";
+        String adjective2 = "what";
+        String adjective3 = "asdf";
+        String verb1 = "zoom";
+        String noun1, noun2, noun3, noun4, noun5, noun6; 
+        noun1 = "apple";
+        noun2 = "fruit";
+        noun3 = "banana";
+        noun4 =  "grape";
+        noun5 = "papaya";
+        noun6 = "papaaya";
+        String name2 = "marchale";
+        int number = 23;
+        String place1 = "wwweblw";
+  
+        
+        //The template for the story
+        String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It is going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+"s ruled the world.";
+  
+        System.out.println(story);
+      }       
+  }
+  
